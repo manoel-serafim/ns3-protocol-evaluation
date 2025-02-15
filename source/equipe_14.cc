aa5354f7e8976ed0aede5c0e42fa5f5c176b01c4
Manoel Serafim <manoel.serafim@proton.me>
2025-02-14 21:39:04 -0300

    (feat) [dev]

--- source/equipe_14.cc
+++ source/equipe_14.cc
@@ -9,146 +9,153 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("EquipeX_Simulacao");
+NS_LOG_COMPONENT_DEFINE("Equipe14_Simulacao");
+
+enum 
+{
+    UDP_PROTO = 0,
+    TCP_PROTO = 1,
+    UDP_TCP_PROTO = 2
+};
+
 
 void RunSimulation(uint32_t nClients,
                    bool mobility,
                    int protocol)
 {
-    NodeContainer serverNode, apNode, clientNodes;
-    serverNode.Create(1);
-    apNode.Create(1);
-    clientNodes.Create(nClients);
+    //pointers to node containers
+    NodeContainer server_node, ap_node, client_nodes;
+    server_node.Create(1UL);
+    ap_node.Create(1UL);
+    client_nodes.Create(nClients);
 
-    // Conexão cabeada entre Servidor e AP
+    // cable connection between server and AP
     PointToPointHelper p2p;
     p2p.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
     p2p.SetChannelAttribute("Delay", StringValue("1ms"));
-    NetDeviceContainer p2pDevices = p2p.Install(serverNode.Get(0), apNode.Get(0));
+    NetDeviceContainer p2p_devices = p2p.Install(server_node.Get(0), ap_node.Get(0));
 
-    // Configuração do Wi-Fi
+    // AP wifi adapter config
     WifiHelper wifi;
     wifi.SetStandard(WIFI_STANDARD_80211ax);
     WifiMacHelper mac;
-    // Substitua esta linha:
-    // YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
-    // Por isso:
+    
     YansWifiPhyHelper phy;
-    phy.Set("RxGain", DoubleValue(0));  // Exemplo de configuração, você pode ajustar conforme necessário
-    phy.Set("TxGain", DoubleValue(0));  // Defina outras propriedades, se necessário
+    phy.Set("RxGain", DoubleValue(0));  
+    phy.Set("TxGain", DoubleValue(0)); 
 
     YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
     phy.SetChannel(channel.Create());
 
-    Ssid ssid = Ssid("EquipeX");
+    Ssid ssid = Ssid("Equipe14");
 
     mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
-    NetDeviceContainer apDevice = wifi.Install(phy, mac, apNode);
+    NetDeviceContainer ap_device = wifi.Install(phy, mac, ap_node);
 
     mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
-    NetDeviceContainer clientDevices = wifi.Install(phy, mac, clientNodes);
+    NetDeviceContainer client_devices = wifi.Install(phy, mac, client_nodes);
 
-    // Configuração da mobilidade
-    MobilityHelper mobilityHelper;
+    // mobility configuration
+    MobilityHelper mobility_helper;
     if (mobility)
     {
-        mobilityHelper.SetPositionAllocator("ns3::RandomRectanglePositionAllocator",
+        mobility_helper.SetPositionAllocator("ns3::RandomRectanglePositionAllocator",
                                             "X", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=50.0]"),
                                             "Y", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=50.0]"));
-        mobilityHelper.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+        mobility_helper.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
                                         "Bounds", RectangleValue(Rectangle(-50, 50, -50, 50)));
     }
     else
     {
-        mobilityHelper.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+        mobility_helper.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     }
-    mobilityHelper.Install(clientNodes);
-    mobilityHelper.Install(apNode);
-    mobilityHelper.Install(serverNode);
+    mobility_helper.Install(client_nodes);
+    mobility_helper.Install(ap_node);
+    mobility_helper.Install(server_node);
 
     // Pilha TCP/IP
     InternetStackHelper stack;
-    stack.Install(serverNode);
-    stack.Install(apNode);
-    stack.Install(clientNodes);
+    stack.Install(server_node);
+    stack.Install(ap_node);
+    stack.Install(client_nodes);
 
     // Endereçamento IP
     Ipv4AddressHelper address;
     address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer serverApIp = address.Assign(p2pDevices);
+    Ipv4InterfaceContainer server_ap_ip = address.Assign(p2p_devices);
 
     address.SetBase("192.168.0.0", "255.255.255.0");
-    Ipv4InterfaceContainer clientIp = address.Assign(clientDevices);
-    Ipv4InterfaceContainer apIp = address.Assign(apDevice);
+    Ipv4InterfaceContainer clientIp = address.Assign(client_devices);
+    Ipv4InterfaceContainer ap_ip = address.Assign(ap_device);
 
     // Configuração das aplicações
     uint16_t port = 9;
-    ApplicationContainer serverApp, clientApp;
+    ApplicationContainer server_app, client_app;
 
-    if (protocol == 0)    // UDP
+    if (protocol == UDP_PROTO)    // UDP
     {
-        UdpEchoServerHelper echoServer(port);
-        serverApp = echoServer.Install(serverNode.Get(0));
-        serverApp.Start(Seconds(1.0));
-        serverApp.Stop(Seconds(10.0));
+        UdpEchoServerHelper echo_server(port);
+        server_app = echo_server.Install(server_node.Get(0));
+        server_app.Start(Seconds(1.0));
+        server_app.Stop(Seconds(10.0));
 
-        UdpEchoClientHelper echoClient(serverApIp.GetAddress(0),
+        UdpEchoClientHelper echo_client(server_ap_ip.GetAddress(0),
                                        port);
-        echoClient.SetAttribute("MaxPackets", UintegerValue(10));
-        echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-        echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+        echo_client.SetAttribute("MaxPackets", UintegerValue(10));
+        echo_client.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+        echo_client.SetAttribute("PacketSize", UintegerValue(1024));
 
         for (uint32_t i = 0; i < nClients; ++i)
         {
-            clientApp.Add(echoClient.Install(clientNodes.Get(i)));
+            client_app.Add(echo_client.Install(client_nodes.Get(i)));
         }
     }
-    else if (protocol == 1)      // TCP
+    else if (protocol == TCP_PROTO)      // TCP
     {
         BulkSendHelper bulkSend("ns3::TcpSocketFactory",
-                                Address(InetSocketAddress(serverApIp.GetAddress(0), port)));
+                                Address(InetSocketAddress(server_ap_ip.GetAddress(0), port)));
         bulkSend.SetAttribute("MaxBytes", UintegerValue(0));
 
         for (uint32_t i = 0; i < nClients; ++i)
         {
-            clientApp.Add(bulkSend.Install(clientNodes.Get(i)));
+            client_app.Add(bulkSend.Install(client_nodes.Get(i)));
         }
 
         PacketSinkHelper sink("ns3::TcpSocketFactory",
                               Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
-        serverApp = sink.Install(serverNode.Get(0));
+        server_app = sink.Install(server_node.Get(0));
     }
-    else      // TCP + UDP
+    else if (protocol == UDP_TCP_PROTO) 
     {
         for (uint32_t i = 0; i < nClients; ++i)
         {
             if (i % 2 == 0)    // UDP
             {
-                UdpEchoClientHelper echoClient(serverApIp.GetAddress(0),
+                UdpEchoClientHelper echo_client(server_ap_ip.GetAddress(0),
                                                port);
-                echoClient.SetAttribute("MaxPackets", UintegerValue(10));
-                echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-                echoClient.SetAttribute("PacketSize", UintegerValue(1024));
-                clientApp.Add(echoClient.Install(clientNodes.Get(i)));
+                echo_client.SetAttribute("MaxPackets", UintegerValue(10));
+                echo_client.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+                echo_client.SetAttribute("PacketSize", UintegerValue(1024));
+                client_app.Add(echo_client.Install(client_nodes.Get(i)));
             }
             else      // TCP
             {
                 BulkSendHelper bulkSend("ns3::TcpSocketFactory",
-                                        Address(InetSocketAddress(serverApIp.GetAddress(0), port)));
+                                        Address(InetSocketAddress(server_ap_ip.GetAddress(0), port)));
                 bulkSend.SetAttribute("MaxBytes", UintegerValue(0));
-                clientApp.Add(bulkSend.Install(clientNodes.Get(i)));
+                client_app.Add(bulkSend.Install(client_nodes.Get(i)));
             }
         }
 
         PacketSinkHelper sink("ns3::TcpSocketFactory",
                               Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
-        serverApp = sink.Install(serverNode.Get(0));
+        server_app = sink.Install(server_node.Get(0));
     }
 
-    clientApp.Start(Seconds(2.0));
-    clientApp.Stop(Seconds(10.0));
-    serverApp.Start(Seconds(1.0));
-    serverApp.Stop(Seconds(10.0));
+    client_app.Start(Seconds(2.0));
+    client_app.Stop(Seconds(10.0));
+    server_app.Start(Seconds(1.0));
+    server_app.Stop(Seconds(10.0));
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
@@ -168,11 +175,11 @@ void RunSimulation(uint32_t nClients,
 int main(int argc,
          char *argv[])
 {
-    LogComponentEnable("EquipeX_Simulacao", LOG_LEVEL_INFO);
+    LogComponentEnable("Equipe14_Simulacao", LOG_LEVEL_ALL);
 
     uint32_t clientCounts[] = { 1, 2, 4, 8, 16, 32 };
     bool mobilityOptions[] = { false, true };
-    int protocolOptions[] = { 0, 1, 2 };  // 0 = UDP, 1 = TCP, 2 = TCP+UDP
+    int protocolOptions[] = { UDP_PROTO, TCP_PROTO, UDP_TCP_PROTO };  
 
     for (bool mobility : mobilityOptions)
     {
@@ -181,7 +188,7 @@ int main(int argc,
             for (uint32_t nClients : clientCounts)
             {
                 NS_LOG_INFO("Simulando " << nClients << " clientes, Mobilidade: " << mobility
-                                         << ", Protocolo: " << (protocol == 0 ? "UDP" : protocol == 1 ? "TCP" : "TCP+UDP"));
+                                         << ", Protocolo: " << (protocol == UDP_PROTO ? "UDP" : protocol == TCP_PROTO ? "TCP" : "TCP+UDP"));
                 RunSimulation(nClients, mobility, protocol);
             }
         }
@@ -189,3 +196,4 @@ int main(int argc,
 
     return 0;
 }
+
